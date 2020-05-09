a7166dd25dc3a53bd8fd43c3b47ecfe83f787221
Aleksandr Nikiforov <alex@alexn.ee>
2020-05-09 14:50:49 +0300

    React/WS example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+const ENDPOINT = "http://127.0.0.1:8080/gs-guide-websocket";
 
 function App() {
+
+  const [stompClient, setStompClient] = useState(null);
+
+  useEffect(() => {
+    //https://medium.com/practo-engineering/websockets-in-react-the-component-way-368730334eef
+    const socket = SockJS(ENDPOINT);
+    const stompClient = Stomp.over(socket);
+    stompClient.connect({}, () => {
+      stompClient.subscribe('/topic/greetings', (data) => {
+        console.log(data);
+      });
+    });
+    setStompClient(stompClient);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <a href='#' onClick={() => {
+        stompClient.send('/app/hello', {}, JSON.stringify({
+          name: 'hi'
+        }))
+      }}>Send message</a>
     </div>
-  );
+  )
+
 }
 
 export default App;
